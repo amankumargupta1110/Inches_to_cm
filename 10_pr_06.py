10886e7fbe003cf3a83e51f5d26f60f339677eb2
AMAN KUMAR GUPTA <114344475+amankumargupta1110@users.noreply.github.com>
2022-10-15 08:10:33 +0530

    Add files via upload

---
+++ 10_pr_06.py
@@ -0,0 +1,15 @@
+# def inch(cm):
+#     return (inch * 2.54)
+
+
+# inch = 2
+# cm = cm(cm)
+# print("centimeter is " + str(cm))
+
+def inch_to_cm():
+    inch = int(input("Enter inches: "))
+    cm = inch * 2.54
+    print(f"{inch} inches = {cm} cm ")
+
+
+inch_to_cm()
